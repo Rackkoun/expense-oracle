79411c0060b64b22cf7cbbc3b976ec5af0c76621
Tchana R <ruphustchana@yahoo.com>
2026-03-03 02:28:16 +0100

    bugfix: ci pull request security check black pass

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,15 +2,19 @@
 This is a basic test for setting up ExpenseOracle
 """
 
+
 def test_imports():
     """Test basic imports work"""
     import sys
+
     assert sys.version_info >= (3, 11)
+
 
 def test_config_loads():
     """Test config"""
     try:
         import config
+
         assert hasattr(config, "APP_NAME")
     except ImportError:
-        pass # config might not exist yet
+        pass  # config might not exist yet
